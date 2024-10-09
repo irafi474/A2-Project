4d24d9033106ddb44febdf6400a2094c0e35b9e8
irafi474 <irafi474@gmail.com>
2024-10-09 14:04:29 +0700

    navbar

--- navbar.php
+++ navbar.php
@@ -17,7 +17,7 @@
             <a class="nav-link text-white active" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">About</a>
+            <a class="nav-link text-white" href="about.php">About</a>
           </li>
           <li class="nav-item">
             <a class="nav-link text-white" href="#">Event</a>
